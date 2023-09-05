dea9a68320447bfb71755cfd6731a5108410ca3c
TaylorLeLievre <jackrabbit5567@gmail.com>
2023-09-04 20:35:12 -0400

    Set up run file and added commander dependancy

---
+++ run.ts
@@ -0,0 +1,20 @@
+//The following dependancies need to be vendored with the software
+
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+    .version("0.0.1")
+    .description("Trustworthy in-house CLI designed for ACME Corp.")
+    .command("install", "Install all dependancies for package manager")
+    .command("test", "Run test suite on codebase")
+
+program
+    .version("0.0.1")
+    .argument("<filename>", "Absolute file path of ASCII-encoded, newline delimited package URLs")
+    .description("Parse package URLs and provide metric scores for each package")
+    .action((filename) => {
+        //open filename
+        console.log("Success")
+    })
