c3b2e3511496807e88e89cc1a878e8dd3dac6fa4
Jack Douglas Gardner <gardne97@eceprog3.ecn.purdue.edu>
2023-09-24 16:38:30 -0400

    fiddling with install

--- install/install.js
+++ install/install.js
@@ -1,30 +1,37 @@
-const {exec, ExecException } = require('child_process');
+const { exec, ExecException } = require('child_process');
 
 const command = `npm install`;
 
 exec(command, (error, stdout, stderr) => {
-if (error) {
+  if (error) {
     console.error(`Error installing dependencies:`, error);
-} else {
-    // Count dependencies before filtering
-    const dependencyCount = (stdout.match(/added \d+ package/g) || []).length;
-
+  } else {
     const lines = stdout.split('\n');
-    const filteredLines = lines.filter(line => {
-    return !(
-        line.includes("up to date, audited") ||
-        line.includes("packages are looking for funding") ||
-        line.includes("found 0 vulnerabilities") ||
-        line.trim() === "run `npm fund` for details"
-    );
+    let dependencyCount = 0;
+
+    lines.forEach((line) => {
+      if (line.includes('added')) {
+        const match = line.match(/added (\d+) package/);
+        if (match) {
+          dependencyCount += parseInt(match[1], 10);
+        }
+      }
     });
 
-    // Display dependency count and filtered output
+    const filteredLines = lines.filter((line) => {
+      return !(
+        line.includes('up to date, audited') ||
+        line.includes('added') ||
+        line.includes('packages are looking for funding') ||
+        line.includes('found 0 vulnerabilities') ||
+        line.trim() === 'run `npm fund` for details'
+      );
+    });
+
     console.log(`${dependencyCount} dependencies installed...`);
     console.log(`${filteredLines.join('\n').trim()}`);
-}
-if (stderr) {
+  }
+  if (stderr) {
     console.error(stderr);
-}
+  }
 });
-
